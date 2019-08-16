88bc038418008a5ec542d116a82b48d3eb23505f
munhee <ansgml9884@naver.com>
2019-08-16 19:38:29 +0900

    heroku edited

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
-release: python3 manage.py migrate --noinput
 web: gunicorn book_service.wsgi --log-file -
 
--- book_service/settings.py
+++ book_service/settings.py
@@ -120,4 +120,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+import dj_database_url db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
 
+
+MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware', 'django.middleware.security.SecurityMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware', 'django.middleware.common.CommonMiddleware', 'django.middleware.csrf.CsrfViewMiddleware', 'django.contrib.auth.middleware.AuthenticationMiddleware', 'django.contrib.messages.middleware.MessageMiddleware', 'django.middleware.clickjacking.XFrameOptionsMiddleware', ]
