6ec9275d82a393c6ed85ae878ca7c7adb8e2ef44
munhee <ansgml9884@naver.com>
2019-08-16 19:55:36 +0900

    first commit

--- book_service/settings.py
+++ book_service/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ah$#p$q#fb3+r1&e6a63#=4y(*9)so$z5wnsk)joc27vk&5mtn'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '(p7p0#$^HHFGHHdggtherververvewkjbnp3290vdvfd)w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['*']
 
